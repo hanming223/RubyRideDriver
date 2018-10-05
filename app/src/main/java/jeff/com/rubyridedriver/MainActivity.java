5a99dc1806285780aa5145a4e1b50d13d2b4f995
hanming223 <hanming223@gmail.com>
2018-10-05 08:39:18 +0800

    ZendDrive integration

--- app/src/main/java/jeff/com/rubyridedriver/MainActivity.java
+++ app/src/main/java/jeff/com/rubyridedriver/MainActivity.java
@@ -3,7 +3,11 @@ package jeff.com.rubyridedriver;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentSender;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Rect;
@@ -18,6 +22,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -31,7 +36,13 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.zendrive.sdk.ZendriveLocationSettingsResult;
 
 import org.w3c.dom.Text;
 
@@ -39,12 +50,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
+import jeff.com.rubyridedriver.utils.Constants;
+import jeff.com.rubyridedriver.utils.TripManager;
+import timber.log.Timber;
+
 public class MainActivity extends AppCompatActivity implements DrawerLayout.DrawerListener {
 
     DrawerLayout drawer;
 
     Runnable mPendingRunnable;
     Handler mHandler;
+
+    private BroadcastReceiver receiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,7 +86,8 @@ public class MainActivity extends AppCompatActivity implements DrawerLayout.Draw
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.menu_icon_color));
 
-        drawer.addDrawerListener(toggle);
+//        drawer.addDrawerListener(toggle);
+        drawer.addDrawerListener(this);
         toggle.syncState();
 
 
@@ -91,6 +109,8 @@ public class MainActivity extends AppCompatActivity implements DrawerLayout.Draw
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         TextView scheduleTextView = (TextView) findViewById(R.id.scheduleTextView);
         TextView historyTextView = (TextView) findViewById(R.id.historyTextView);
+
+        RelativeLayout clockInOutLayout = (RelativeLayout)findViewById(R.id.clockInOutLayout);
 
         Fragment fragment = null;
         Class fragmentClass;
@@ -114,10 +134,6 @@ public class MainActivity extends AppCompatActivity implements DrawerLayout.Draw
                 TextView titleView = (TextView)findViewById(R.id.titleView);
                 titleView.setText("Your Schedule");
 
-
-
-                drawer.closeDrawers();
-
                 mPendingRunnable = new Runnable() {
                     @Override
                     public void run() {
@@ -140,6 +156,7 @@ public class MainActivity extends AppCompatActivity implements DrawerLayout.Draw
                 };
 
 
+                drawer.closeDrawers();
             }
         });
 
@@ -149,9 +166,6 @@ public class MainActivity extends AppCompatActivity implements DrawerLayout.Draw
 
                 TextView titleView = (TextView)findViewById(R.id.titleView);
                 titleView.setText("Your Past Trips");
-
-                drawer.closeDrawers();
-
 
                 mPendingRunnable = new Runnable() {
                     @Override
@@ -174,18 +188,122 @@ public class MainActivity extends AppCompatActivity implements DrawerLayout.Draw
                     }
                 };
 
+                drawer.closeDrawers();
+
+            }
+        });
+
+        clockInOutLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                drawer.closeDrawers();
+
+                Timber.i("offDutyButton tapped");
+                TripManager.sharedInstance().goOffDuty();
+
             }
         });
 
         mHandler = new Handler();
 
+        registerForBroadcasts();
+    }
+
+    private void registerForBroadcasts() {
+        LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.
+                getInstance(this.getApplicationContext());
+        receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                processIntent(intent);
+            }
+        };
+        localBroadcastManager.registerReceiver(receiver, getIntentFilterForLocalBroadcast());
+    }
+
+    private static IntentFilter getIntentFilterForLocalBroadcast() {
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Constants.LOCATION_PERMISSION_CHANGE_BROADCAST);
+        intentFilter.addAction(Constants.LOCATION_SETTINGS_CHANGE_BROADCAST);
+        return intentFilter;
+    }
+
+    private void processIntent(Intent intent) {
+        String action = intent.getAction();
+        if (action != null) {
+            switch (action) {
+                case Constants.LOCATION_SETTINGS_CHANGE_BROADCAST:
+                    ZendriveLocationSettingsResult result =
+                            intent.getParcelableExtra(Constants.DATA);
+                    if (result != null && !result.isSuccess()) {
+                        resolveLocationSettings(this, result);
+                    }
+                    // Maybe ask user to re-enable the location settings.
+                    break;
+                case Constants.LOCATION_PERMISSION_CHANGE_BROADCAST:
+                    boolean granted =
+                            intent.getBooleanExtra(Constants.LOCATION_PERMISSION_CHANGE_BROADCAST,
+                                    false);
+                    requestLocationPermission(granted);
+                    break;
+            }
+        }
+    }
+
+    private void requestLocationPermission(boolean granted) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!granted) {
+                requestPermissions(
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                        Constants.LOCATION_PERMISSION_REQUEST);
+            }
+        }
+    }
+
+    private void resolveLocationSettings(Activity activity, ZendriveLocationSettingsResult locationSettingsResult) {
+        if (locationSettingsResult.isSuccess()) {
+            return;
+        }
+        for (ZendriveLocationSettingsResult.Error error : locationSettingsResult.errors) {
+            switch (error) {
+                case GOOGLE_PLAY_SERVICES_ERROR_RESULT: {
+                    LocationSettingsResult result = locationSettingsResult.locationSettingsResultFromGooglePlayService;
+                    final Status status = result.getStatus();
+                    switch (status.getStatusCode()) {
+                        case LocationSettingsStatusCodes.SUCCESS:
+                            // Should not happen
+                            Timber.e("Success received when expected" +
+                                    "error from Google Play " +
+                                    "Services");
+                            break;
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                            // Location settings are not satisfied. But could be fixed by showing the user
+                            // a dialog.
+                            try {
+                                // Show the dialog by calling startResolutionForResult(),
+                                // and check the result in onActivityResult().
+                                status.startResolutionForResult(
+                                        activity,
+                                        Constants.GOOGLE_PLAY_SERVICES_REQUEST_CHECK_SETTINGS);
+                            } catch (IntentSender.SendIntentException e) {
+                                // Ignore the error.
+                            }
+                            break;
+                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                            // Location settings are not satisfied. However, we have no way to fix the
+                            // settings so we won't show the dialog.
+                            break;
+                    }
+                }
+            }
+        }
     }
 
     @Override
     public void onBackPressed() {
 
     }
-
 
     @Override
     public void onDrawerSlide(@NonNull View view, float v) {
@@ -210,5 +328,11 @@ public class MainActivity extends AppCompatActivity implements DrawerLayout.Draw
     @Override
     public void onDrawerStateChanged(int i) {
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        LocalBroadcastManager.getInstance(this.getApplicationContext()).unregisterReceiver(receiver);
     }
 }
--- app/src/main/java/jeff/com/rubyridedriver/ScheduleFragment.java
+++ app/src/main/java/jeff/com/rubyridedriver/ScheduleFragment.java
@@ -13,10 +13,15 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+
+import jeff.com.rubyridedriver.utils.TripManager;
+import timber.log.Timber;
 
 
 public class ScheduleFragment extends Fragment {
@@ -24,11 +29,16 @@ public class ScheduleFragment extends Fragment {
     private Button clockinButton;
     private LinearLayout topView;
 
+    LinearLayout contentView;
+
     private List<TaskModel> taskArray = new ArrayList<TaskModel>();
     private List<TaskView> taskViewArray = new ArrayList<TaskView>();
 
     public Integer activeTaskIndex = 0;
     public View fragmentView;
+
+    RelativeLayout clockOutLayout, clockOutConfirmLayout;
+    LinearLayout shiftOverLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
@@ -45,12 +55,12 @@ public class ScheduleFragment extends Fragment {
 
         topView = (LinearLayout) fragmentView.findViewById(R.id.topView);
 
+        shiftOverLayout = (LinearLayout)fragmentView.findViewById(R.id.shiftOverLayout);
+
         clockinButton = (Button) fragmentView.findViewById(R.id.clockinButton);
         clockinButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                topView.setVisibility(View.GONE);
 
                 onClockInButtonClicked();
 
@@ -58,7 +68,7 @@ public class ScheduleFragment extends Fragment {
         });
 
 
-        LinearLayout contentView = (LinearLayout)fragmentView.findViewById(R.id.contentView);
+        contentView = (LinearLayout)fragmentView.findViewById(R.id.contentView);
 
         String[] nameArray = {"Brian Flaherty", "Brian Flaherty", "Maggie Gyllenthal", "Giovanna Bologna", "Maggie Gyllenthal"};
         Integer[] typeAray = {0, 1, 0, 1, 0};
@@ -86,12 +96,52 @@ public class ScheduleFragment extends Fragment {
         }
 
 
+        //clock out action
+        clockOutLayout = (RelativeLayout) view.findViewById(R.id.clockOutLayout);
+        clockOutConfirmLayout = (RelativeLayout) view.findViewById(R.id.clockOutConfirmLayout);
+        ImageView clockOutButton = (ImageView) view.findViewById(R.id.clockOutButton);
+        TextView clockOutConfirmButton = (TextView) view.findViewById(R.id.clockOutConfirmButton);
+
+        clockOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (clockOutConfirmLayout.getVisibility() == View.GONE){
+                    clockOutConfirmLayout.setVisibility(View.VISIBLE);
+                }else{
+                    clockOutConfirmLayout.setVisibility(View.GONE);
+                }
+
+            }
+        });
+
+        clockOutConfirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                onClockOutButtonClicked();
+
+            }
+        });
+
+        Button shiftOverClockOutButton = (Button)view.findViewById(R.id.shiftOverButton);
+        shiftOverClockOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                onClockOutButtonClicked();
+
+            }
+        });
+
     }
 
 
     public void onClockInButtonClicked(){
 
         AppManager.getInstance().isClockedIn = true;
+
+        topView.setVisibility(View.GONE);
 
         taskViewArray.get(0).startEndButtonView.setVisibility(View.VISIBLE);
         AppManager.getInstance().expand(taskViewArray.get(0).conetentView);
@@ -119,7 +169,6 @@ public class ScheduleFragment extends Fragment {
             });
         }
 
-
         //make name clickable
 
         if (taskArray.get(0).requestType == 0){
@@ -136,6 +185,60 @@ public class ScheduleFragment extends Fragment {
             });
         }
 
+        clockOutLayout.setVisibility(View.VISIBLE);
+
+        //insurance SDK
+
+        Timber.i("goOnDutyButtonClicked");
+        TripManager.sharedInstance().goOnDuty();
+
+    }
+
+    public void onClockOutButtonClicked(){
+
+        topView.setVisibility(View.VISIBLE);
+
+        AppManager.getInstance().isClockedIn = false;
+
+        clockOutLayout.setVisibility(View.GONE);
+
+        shiftOverLayout.setVisibility(View.GONE);
+        clockOutConfirmLayout.setVisibility(View.GONE);
+
+        activeTaskIndex = 0;
+
+        //remove all subview in contentview
+
+        contentView.removeAllViews();
+        taskArray.clear();
+        taskViewArray.clear();
+
+
+        String[] nameArray = {"Brian Flaherty", "Brian Flaherty", "Maggie Gyllenthal", "Giovanna Bologna", "Maggie Gyllenthal"};
+        Integer[] typeAray = {0, 1, 0, 1, 0};
+
+        for(int i = 0; i < nameArray.length; i++ ){
+
+            TaskModel task = new TaskModel();
+
+            task.name = nameArray[i];
+            task.requestType = typeAray[i];
+            task.address = "1622 morningside ave";
+            task.distance = "4.7 miles";
+            task.period = "8 mins";
+            task.requestedDate = "Requested on Tuesday at 11.15PM";
+            task.instruction = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.";
+
+            taskArray.add(task);
+
+            TaskView taskView = new TaskView(getActivity(), this, task);
+            taskView.setName(task.name);
+            taskView.myIndex = i;
+            contentView.addView(taskView);
+
+            taskViewArray.add(taskView);
+        }
+
     }
 
     public void goToNextTask(){
@@ -150,7 +253,6 @@ public class ScheduleFragment extends Fragment {
 
         //check if shift is over
         if (activeTaskIndex >= (taskArray.size() - 1)){
-            LinearLayout shiftOverLayout = (LinearLayout)fragmentView.findViewById(R.id.shiftOverLayout);
             shiftOverLayout.setVisibility(View.VISIBLE);
             return;
         }
@@ -160,9 +262,13 @@ public class ScheduleFragment extends Fragment {
 
         taskViewArray.get(activeTaskIndex).startEndButtonView.setVisibility(View.VISIBLE);
         if (taskArray.get(activeTaskIndex).requestType == 0){
+
             taskViewArray.get(activeTaskIndex).startEndButton.setText("START PICK UP");
+
         }else{
+
             taskViewArray.get(activeTaskIndex).startEndButton.setText("START DROP");
+
         }
 
         taskViewArray.get(activeTaskIndex).headerBackground.setVisibility(View.INVISIBLE);
